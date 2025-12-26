33d1d730a89dfc368de4cc2a632c8d69b4f0da06
starx04 <antonifernandez207@gmail.com>
2025-12-25 21:42:04 -0500

    proyecto

--- licoreria/views.py
+++ licoreria/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Producto
 
-# Create your views here.
+def index(request):
+    productos = Producto.objects.all()
+    return render(request, 'licoreria/index.html', {'productos': productos})
